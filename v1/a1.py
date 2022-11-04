8fae7659f163c0ee1140e24801d493d21988714a
Ostabo <osi.borkenhagen@gmail.com>
2022-11-04 14:55:38 +0100

    firs working plot of mean and std

--- v1/a1.py
+++ v1/a1.py
@@ -2,18 +2,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Array from 10 to 70 with step 3
-dis = np.arange(10, 70, 3)
+dis = np.arange(10, 71, 3)
+
+analog_data = dict(np.genfromtxt('./data/messungen_v2', delimiter=';', dtype=[int, float]))
 
 avg_data = dict()
 sdt_data = dict()
 for x in dis:
-    raw_data = [[s.replace(',', '.') for s in e] for e in np.genfromtxt('./data/' + str(x) + 'cm.csv', delimiter=';', skip_header=1000, dtype=str)]
+    raw_data = [float(s.replace(',', '.')) for s in
+                np.genfromtxt('./data/' + str(x) + 'cm.csv',
+                              delimiter=';',
+                              skip_header=1000,
+                              usecols=1,
+                              dtype=str)]
     avg_data[x] = np.mean(raw_data, dtype=float)
     sdt_data[x] = np.std(raw_data, dtype=float)
 
 # Plotting
-plt.errorbar(avg_data.keys(), avg_data.values(), yerr=sdt_data.values(), fmt='o')
+fig, axs = plt.subplots(2, sharex=True)
+fig.suptitle('Mean and std of the data')
+axs[0].plot(avg_data.keys(), avg_data.values(), 'r', label='Mean')
+axs[0].plot(analog_data.keys(), analog_data.values(), 'go', label='Analog')
+axs[1].plot(sdt_data.keys(), sdt_data.values(), 'b', label='Std')
+# x label
 plt.xlabel('Distance (cm)')
-plt.ylabel('Voltage (V)')
-plt.title('Voltage vs Distance')
+# y label
+axs[0].set(ylabel='Voltage (V)')
+axs[1].set(ylabel='Voltage (V)')
+fig.legend()
+
 plt.show()
