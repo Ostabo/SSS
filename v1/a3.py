d7f6aea79a7420728a49afa130b1be9749d3379e
Ostabo <osi.borkenhagen@gmail.com>
2022-11-09 09:20:23 +0100

    a3 idk

--- v1/a3.py
+++ v1/a3.py
@@ -37,12 +37,12 @@ print('68% Length: ' + str(perc_95_l))
 print('95% Length: ' + str(perc_95_l))
 
 # Funktionen zur Berechnung
-a1 = np.log(29.7) / np.log(np.mean(raw_data_width))
-b1 = np.log(29.7) - (a1 * np.log(np.mean(raw_data_width)))
-z1 = (np.exp(b1) * a1 * np.mean(raw_data_width) ** (a1 - 1))
+VtoCm = np.log(29.7) / np.log(np.mean(raw_data_width))  # Faktor von V zu cm
+zero = np.log(29.7) - (VtoCm * np.log(np.mean(raw_data_width)))  # -> 0
+z1 = (np.exp(zero) * VtoCm * np.mean(raw_data_width) ** (VtoCm - 1))  # exp(0) = 1, ableitung von e^b * x^a
 
 # e^b * x^a  -  Umkehrung der doppelten Logarithmierung
-y1 = np.exp(b1) * np.power(np.mean(raw_data_width), a1)
+y1 = np.exp(zero) * np.power(np.mean(raw_data_width), VtoCm)
 
 # Berechnung von 21 cm Messfehler #
 a2 = np.log(21) / np.log(np.mean(raw_data_length))
@@ -55,11 +55,15 @@ y2 = np.exp(b2) * np.power(np.mean(raw_data_length), a2)
 # Errechnung des Flächeninhalts
 flache = round(y2, 2) * round(y1, 2)
 # Ausgeben der errechneten Messkorrekturen für die jeweilige Gaußverteilung
-print("y(68%) = " + str(round(y2, 2)) + " cm +- " + str(np.abs(perc_68_w)) + " cm")
-print("y(95%) = " + str(round(y2, 2)) + " cm +- " + str(np.abs(perc_95_w)) + " cm")
+print()
+print("Length(68%) = " + str(round(y1, 2)) + " cm +- " + str(np.abs(perc_68_l)) + " cm")
+print("Length(95%) = " + str(round(y1, 2)) + " cm +- " + str(np.abs(perc_95_l)) + " cm")
+print()
+print("Width(68%) = " + str(round(y2, 2)) + " cm +- " + str(np.abs(perc_68_w)) + " cm")
+print("Width(95%) = " + str(round(y2, 2)) + " cm +- " + str(np.abs(perc_95_w)) + " cm")
 print()
 print("Ein DinA4 Blatt hat ein Flächeninhalt von " + str(round(flache, 2)) + " cm^2")
 print("68% hat bei einem Flächeninhalt von " + str(round(flache, 2)) + "cm^2 einen Messfehler von +" + str(
-    (perc_68_l + perc_68_l)) + "cm")
+    (perc_68_l + perc_68_w)) + "cm")
 print("95% hat bei einem Flächeninhalt von " + str(round(flache, 2)) + "cm^2 einen Messfehler von +" + str(
-    (perc_95_l + perc_95_l)) + "cm")
+    (perc_95_l + perc_95_w)) + "cm")
