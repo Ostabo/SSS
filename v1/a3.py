e1bb4f6e76e60faff1b99427387c3190875395fa
Ostabo <osi.borkenhagen@gmail.com>
2022-11-09 10:31:40 +0100

    a3 twisted length and width

--- v1/a3.py
+++ v1/a3.py
@@ -37,16 +37,16 @@ print('68% Length: ' + str(perc_95_l))
 print('95% Length: ' + str(perc_95_l))
 
 # Funktionen zur Berechnung
-VtoCm = np.log(29.7) / np.log(np.mean(raw_data_width))  # Faktor von V zu cm
-zero = np.log(29.7) - (VtoCm * np.log(np.mean(raw_data_width)))  # -> 0
+VtoCm = np.log(21) / np.log(np.mean(raw_data_width))  # Faktor von V zu cm
+zero = np.log(21) - (VtoCm * np.log(np.mean(raw_data_width)))  # -> 0
 z1 = (np.exp(zero) * VtoCm * np.mean(raw_data_width) ** (VtoCm - 1))  # exp(0) = 1, ableitung von e^b * x^a
 
 # e^b * x^a  -  Umkehrung der doppelten Logarithmierung
 y1 = np.exp(zero) * np.power(np.mean(raw_data_width), VtoCm)
 
 # Berechnung von 21 cm Messfehler #
-a2 = np.log(21) / np.log(np.mean(raw_data_length))
-b2 = np.log(21) - (a2 * np.log(np.mean(raw_data_length)))
+a2 = np.log(29.7) / np.log(np.mean(raw_data_length))
+b2 = np.log(29.7) - (a2 * np.log(np.mean(raw_data_length)))
 z2 = (np.exp(b2) * a2 * np.mean(raw_data_length) ** (a2 - 1))
 
 # e^b * x^a  -  Umkehrung der doppelten Logarithmierung
@@ -56,11 +56,11 @@ y2 = np.exp(b2) * np.power(np.mean(raw_data_length), a2)
 flache = round(y2, 2) * round(y1, 2)
 # Ausgeben der errechneten Messkorrekturen für die jeweilige Gaußverteilung
 print()
-print("Length(68%) = " + str(round(y1, 2)) + " cm +- " + str(np.abs(perc_68_l)) + " cm")
-print("Length(95%) = " + str(round(y1, 2)) + " cm +- " + str(np.abs(perc_95_l)) + " cm")
+print("Width(68%) = " + str(round(y1, 2)) + " cm +- " + str(np.abs(perc_68_w)) + " cm")
+print("Width(95%) = " + str(round(y1, 2)) + " cm +- " + str(np.abs(perc_95_w)) + " cm")
 print()
-print("Width(68%) = " + str(round(y2, 2)) + " cm +- " + str(np.abs(perc_68_w)) + " cm")
-print("Width(95%) = " + str(round(y2, 2)) + " cm +- " + str(np.abs(perc_95_w)) + " cm")
+print("Length(68%) = " + str(round(y2, 2)) + " cm +- " + str(np.abs(perc_68_l)) + " cm")
+print("Length(95%) = " + str(round(y2, 2)) + " cm +- " + str(np.abs(perc_95_l)) + " cm")
 print()
 print("Ein DinA4 Blatt hat ein Flächeninhalt von " + str(round(flache, 2)) + " cm^2")
 print("68% hat bei einem Flächeninhalt von " + str(round(flache, 2)) + "cm^2 einen Messfehler von +" + str(
