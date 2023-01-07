c2604576effa28d592ee4f45f8aa5429b5b424e0
Ostabo <osi.borkenhagen@gmail.com>
2023-01-07 10:19:26 +0100

    mach aufnahme

--- v4/a2.py
+++ v4/a2.py
@@ -1,21 +1,29 @@
 from a1 import record, time, numpy, trigger, windowing, plt
 
 
-def do_record(current):
+def do_record(current, ref_add):
     for j in range(0, 5):
         time.sleep(2)
         print(f"ready for run {j} - {current}")
         rec = record()
-        numpy.save(f"recorded/a2/{current}_{j}.npy", trigger(rec, 2000))
+        numpy.save(f"recorded/a2/{current}_{j}{ref_add}.npy", trigger(rec, 2000))
         print("2s pause")
 
 
 TASKS = ['hoch', 'runter', 'links', 'rechts']
 
-do_record(TASKS[0])
-do_record(TASKS[1])
-do_record(TASKS[2])
-do_record(TASKS[3])
+# do_record(TASKS[0], '')
+# do_record(TASKS[1], '')
+# do_record(TASKS[2], '')
+# do_record(TASKS[3], '')
+# do_record(TASKS[0], '_ref')
+# do_record(TASKS[1], '_ref')
+# do_record(TASKS[2], '_ref')
+# do_record(TASKS[3], '_ref')
+do_record(TASKS[0], '_ref_lars')
+do_record(TASKS[1], '_ref_lars')
+do_record(TASKS[2], '_ref_lars')
+do_record(TASKS[3], '_ref_lars')
 
 for t in TASKS:
     print(t)
